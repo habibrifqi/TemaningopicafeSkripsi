4097cf0fd63bdf1fe880ab468ab6cf53b8485599
Your Name <habib.rifqi.16@gmail.com>
2022-02-22 20:54:24 +0700

    donwload laravel + admin lte 3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/master')
+
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="breadcrumb-item active">Dashboard</li>
+@endsection
