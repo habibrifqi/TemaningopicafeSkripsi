fb6a7d753abb497d32e88ba73c403b6b34e48ea1
Your Name <habib.rifqi.16@gmail.com>
2022-02-23 14:13:08 +0700

    membuat sedder menu

---
+++ database/migrations/2022_02_22_143350_create_menu_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_kategori')->nullable();
+            $table->string('nama_menu')->unique();
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+};
